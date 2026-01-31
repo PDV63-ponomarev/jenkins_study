255969ce31f9c4d2a14804b310b25ab907de629e
Dima <graiwers@gmail.com>
2026-01-31 15:38:40 +0400

    change test

--- conftest.py
+++ conftest.py
@@ -1,33 +1,13 @@
 import pytest
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selene import Browser, Config
+
 
 # from utils import attach
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
 
-    # options = Options()
-    # capabilselenoid_capabilitieities = {
-    #     "browserName": "chrome",
-    #     "browserVersion": "128.0",
-    #     "selenoid:options": {
-    #         "enableVNC": True,
-    #         "enableVideo": True
-    #     }
-    # }
-    #
-    # options.capabilities.update(selenoid_capabilitie)
-    # driver = webdriver.Remote(
-    #     command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #     desired_capabilities=capabilities)
-    #
-    # browser = Browser(Config(driver))
-    # yield browser
-    #
-    # browser.quit()
 
 
     capabilities = {
@@ -41,6 +21,8 @@ def setup_browser(request):
     driver = webdriver.Remote(
         command_executor="https://selenoid.autotests.cloud/wd/hub",
         desired_capabilities=capabilities)
+
+    driver.maximize_window()
 
     yield browser
     browser.quit()
